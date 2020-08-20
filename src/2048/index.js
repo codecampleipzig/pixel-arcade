6a33283dbd5344e626302784d85082ab78e443bc
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-08-20 09:49:56 +0200

    Remove squashRow

--- src/2048/index.js
+++ src/2048/index.js
@@ -1,13 +1,1 @@
-export const squashRow = row => {
-  const filledFields = row.filter(number => number != 0);
-
-  for (let i = 1; i < filledFields.length; ++i) {
-    if (filledFields[i - 1] == filledFields[i]) {
-      filledFields[i - 1] *= 2;
-      filledFields[i] = 0;
-    }
-  }
-  const squashedRow = filledFields.filter(number => number != 0);
-
-  return [...squashedRow, ...Array(4 - squashedRow.length).fill(0)];
-};
+export const squashRow = row => {};
